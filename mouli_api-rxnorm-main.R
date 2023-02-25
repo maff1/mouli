24e20d3b23fe1b8972503a7361f7cb0c85c47f01
maff1 <marco@marcof-7430.well.ox.ac.uk>
2023-02-25 13:10:10 +0000

    changed cols headers

--- mouli_api-rxnorm-main.R
+++ mouli_api-rxnorm-main.R
@@ -1,4 +1,3 @@
-
 #' Get approximate match from string
 #'
 #' @param rx_string string to search for.
@@ -10,19 +9,30 @@
 #' @export
 #'
 #' @examples
-#' get_approximate_match(c("Piribedil","zocor"), 2)
+#' get_approximate_match(c("Piribedil","zocor", "levodopa"), 2)
 
 get_approximate_match <- function(rx_string, max_entries) {
   check_internet()
   cols_order = c("rxcui", "rxaui", "score", "rank", "name", "source")
   for(rx in 1:length(rx_string)) {
-    df <- parse_rx_name(httr::GET(
+    df <- cbind(rx_string[rx],
+      parse_rx_name(httr::GET(
       paste0(
         base_url, "approximateTerm.json?term=", rx_string[rx], "&maxEntries=", max_entries
         )
       )
-    )[, cols_order]
-    write.table(df, file="rxfile.txt", append=TRUE, row.names = F)
+    )[, cols_order])
+    if(rx != 1) {
+      write.table(df, file="rxfile.csv", append=TRUE, row.names = F, col.names = F, 
+                  sep = ",", qmethod = "double", fileEncoding = "utf-8")
+    } else {
+      write.table(df, file="rxfile.csv", append=TRUE, row.names = F, col.names = c("input", cols_order), 
+                  sep = ",", qmethod = "double", fileEncoding = "utf-8")
+    }
   Sys.sleep(1)
   }
 }
+
+
+
+
